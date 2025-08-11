d158df6f6ae00feb7ae9682701a15537c98c26f0
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-08-11 09:05:05 +0200

    new zenoh config

--- scripts/set_zenoh_config.sh
+++ scripts/set_zenoh_config.sh
@@ -1,3 +1,7 @@
 export ROS_DOMAIN_ID=100
-export ZENOH_ROUTER_CONFIG_URI=$HOME/repos/crisp_py/config/rmw/humble_zenoh_router_config.json5
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+
+export ZENOH_CONFIG_OVERRIDE='mode="client";connect/endpoints=["tcp/10.157.163.20:7447"]'
+export RUST_LOG=zenoh=info
+
+ros2 daemon stop && ros2 daemon start
