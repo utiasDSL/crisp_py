a1159b660a4843298b93b078e7ebc2b5723400f5
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2026-01-07 16:12:40 +0100

    viser example

--- examples/12_viser_example.py
+++ examples/12_viser_example.py
@@ -9,9 +9,13 @@ import viser
 from viser.extras import ViserUrdf
 
 from crisp_py.robot import make_robot
+from crisp_py.utils.geometry import Pose
 
 robot = make_robot("fr3")
 robot.wait_until_ready()
+
+robot.home()
+pose_home = robot.end_effector_pose
 
 robot.controller_switcher_client.switch_controller("cartesian_impedance_controller")
 robot.cartesian_controller_parameters_client.load_param_config(
@@ -79,6 +83,21 @@ def main(
             trimesh_scene.bounds[0, 2] if trimesh_scene is not None else 0.0,
         ),
     )
+    # Add interactive transform controls for the end effector.
+    transform_handle = server.scene.add_transform_controls(
+        "/end_effector_target",
+        position=pose_home.position,
+        scale=0.3,
+        line_width=3.0,
+    )
+
+    # Add callback for when the transform handle is moved.
+    @transform_handle.on_update
+    def update_robot_target(handle: viser.TransformControlsEvent) -> None:
+        # Get target pose from the handle.
+        pose = Pose(position=handle.target.position, orientation=pose_home.orientation)
+        robot.set_target(pose=pose)
+
 
     # Sleep forever.
     while True:
